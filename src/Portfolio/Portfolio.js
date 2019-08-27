aded039a0bb2aacb2a5806b925bcf235a73f5a32
Tarin Strosser <strawsir@gmail.com>
2019-08-26 21:26:41 -0600

    more styling. made mockup of portfolio section

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -3,7 +3,24 @@ import './Portfolio.css'
 function Portfolio() {
   return (
     <div className="Portfolio">
-     PORTFOLIO
+
+     <div className="project">
+        <div className="projectimage"> project image</div>
+        this is a description of my project, as well as what was used to build it!
+      </div>
+     <div className="project">
+        <div className="projectimage"> project image</div>
+        this is a description of my project, as well as what was used to build it!
+      </div>
+     <div className="project">
+        <div className="projectimage"> project image</div>
+        this is a description of my project, as well as what was used to build it!
+      </div>
+     <div className="project">
+        <div className="projectimage"> project image</div>
+        this is a description of my project, as well as what was used to build it!
+      </div>
+
     </div>
   );
 }
